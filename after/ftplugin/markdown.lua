b9cc3b62e58a06376ba65b038aa3688fd486dcb5
PhatLe <phatzai2003@gmail.com>
2025-06-17 23:16:08 +0700

    style: change colorscheme and highlight color column

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,5 +4,11 @@ set.textwidth = 90
 set.spell = false
 set.linebreak = true
 
+vim.cmd("colorscheme tokyonight-moon")
+
 vim.wo.colorcolumn = "95"
+
 vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#73e4f5" })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#808080", bold = true })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff", bg = "#616360" }) -- Highlight other line numbers
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#808080", bold = true })
