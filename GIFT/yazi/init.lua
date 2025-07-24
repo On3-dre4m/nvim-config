2f4d2c337994c98a879763f158048f6a98dad179
PhatLe <phatzai2003@gmail.com>
2025-07-24 21:53:25 +0700

    UPDATE: Configuration of Dotfiles as a gift
    - Tmux
    - Yazi
    - Kitty
    - Zed IDE

---
+++ GIFT/yazi/init.lua
@@ -0,0 +1,56 @@
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+})
+
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+require("custom-shell"):setup({
+	history_path = "default",
+	save_history = true,
+})
+
+require("yatline"):setup({
+	show_background = false,
+
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "coloreds", custom = false, name = "count" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+})
+
+require("starship"):setup({
+	hide_flags = false, -- Default: false
+	-- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
+	flags_after_prompt = true, -- Default: true
+	-- Custom starship configuration file to use
+	config_file = "~/.config/starship.toml", -- Default: nil
+})
